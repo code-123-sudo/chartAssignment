c27ca8dec1c93e72b46934f13e51731d8e48a871
code-123-sudo <shishirjain899@gmail.com>
2020-08-26 20:45:51 +0530

    moved weather display functionality to separate component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import WeatherChart from './components/weather';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Legend, Category, Tooltip, DataLabel, LineSeries } from '@syncfusion/ej2-react-charts';
 // data in original format to be passed through api
 import { days, max , min , average } from './mockData/data';
@@ -9,23 +10,9 @@ import { weatherData } from './mockData/data';
 class App extends React.Component {
     constructor() {
         super(...arguments);
-        this.data = weatherData;
-        this.primaryxAxis = { valueType: 'Category', title: 'Days' };
-        this.primaryyAxis = { minimum: 0, maximum: 80, interval: 20, title: 'Temperature' };
     }
     render() {
-        console.log(weatherData)
-        return <ChartComponent id='charts' primaryXAxis={this.primaryxAxis} primaryYAxis={this.primaryyAxis} title='Temperature Chart'>
-                    <Inject services={[ColumnSeries, Legend, Tooltip, DataLabel, LineSeries, Category]}/>
-                        <SeriesCollectionDirective>
-                            <SeriesDirective dataSource={this.data} xName='day' yName='max' type='Column'>
-                            </SeriesDirective>
-                            <SeriesDirective dataSource={this.data} xName='day' yName='min' type='Column'>
-                            </SeriesDirective>
-                        <SeriesDirective dataSource={this.data} xName='day' yName='average' type='Column'>
-                    </SeriesDirective>
-                    </SeriesCollectionDirective>
-    </ChartComponent>;
+        return <div><WeatherChart /></div>
     }
 };
 
---
+++ my-app/src/components/weather.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Legend, Category, Tooltip, DataLabel, LineSeries } from '@syncfusion/ej2-react-charts';
+// data in original format to be passed through api
+import { days, max , min , average } from '../mockData/data';
+//data in required format after passing through api
+import { weatherData } from '../mockData/data';
+
+class WeatherChart extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.data = weatherData;
+        this.primaryxAxis = { valueType: 'Category', title: 'Days' };
+        this.primaryyAxis = { minimum: 0, maximum: 80, interval: 20, title: 'Temperature' };
+    }
+    render() {
+        return <ChartComponent id='charts' primaryXAxis={this.primaryxAxis} primaryYAxis={this.primaryyAxis} title='Temperature Chart'>
+                    <Inject services={[ColumnSeries, Legend, Tooltip, DataLabel, LineSeries, Category]}/>
+                        <SeriesCollectionDirective>
+                            <SeriesDirective dataSource={this.data} xName='day' yName='max' type='Column'>
+                            </SeriesDirective>
+                            <SeriesDirective dataSource={this.data} xName='day' yName='min' type='Column'>
+                            </SeriesDirective>
+                        <SeriesDirective dataSource={this.data} xName='day' yName='average' type='Column'>
+                    </SeriesDirective>
+                    </SeriesCollectionDirective>
+    </ChartComponent>;
+    }
+};
+
+export default WeatherChart;
