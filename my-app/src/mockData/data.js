6a460e74d3685d7bc1f182cdda44aebd5b24adf2
code-123-sudo <shishirjain899@gmail.com>
2020-08-26 20:23:49 +0530

    innitial commit

---
+++ my-app/src/mockData/data.js
@@ -0,0 +1,52 @@
+export const   days = 	[
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+
+export const  max  = 	[
+ 			 50,
+             40,
+             70,
+             60,
+             50,
+             40,
+             40,
+        ];
+
+export const  min = 	[
+         	 50,
+             40,
+             70,
+             60,
+             50,
+             40,
+             40,
+        ];
+
+
+export const  average = 	[
+         	 50,
+             40,
+             70,
+             60,
+             50,
+             40,
+             40,
+        ];
+
+
+// an api will convert the above data in three arrays to required below data
+export const weatherData = [
+            { day: "Sunday", max: 50, min: 70, average: 45 },
+            { day: "Monday", max: 40, min: 60, average: 55 },
+            { day: "Tuesday", max: 70, min: 60, average: 50 },
+            { day: "Wednesday", max: 60, min: 56, average: 40 },
+            { day: "Thursday", max: 50, min: 45, average: 35 },
+            { day: "Friday", max: 40, min: 30, average: 22 },
+            { day: "Saturday", max: 40, min: 35, average: 37 },
+        ];
