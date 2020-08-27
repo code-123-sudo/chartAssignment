2841ca6f7fcd0acc7c73315a06dc8bcf0f47d45d
code-123-sudo <shishirjain899@gmail.com>
2020-08-27 12:43:23 +0530

    removed extra imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import WeatherChart from './components/weather';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, ColumnSeries, Legend, Category, Tooltip, DataLabel, LineSeries } from '@syncfusion/ej2-react-charts';
-// data in original format to be passed through api
-import { days, max , min , average } from './mockData/data';
-//data in required format after passing through api
-import { weatherData } from './mockData/data';
 
 class App extends React.Component {
     constructor() {
